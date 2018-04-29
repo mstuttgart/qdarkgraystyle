9adf94662f47f7b6626c358025d157caa0def227
Michell Stuttgart <michellstut@gmail.com>
2018-04-29 10:23:21 -0300

    [IMP] Drop support to python 3.2, 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -58,9 +58,6 @@ setup(
           'Operating System :: POSIX :: Linux',
           'Operating System :: MacOS',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Topic :: Software Development :: Libraries :: Application '
           'Frameworks',
