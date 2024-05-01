6e64b0d682cc597c3c6eb2809b112dd535680eea
Michell Stuttgart <michellstut@gmail.com>
2024-05-01 16:52:19 -0300

    Format test code

--- tests/test_example_pyqt5.py
+++ tests/test_example_pyqt5.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
-#
-# The MIT License (MIT)
-#
-# Copyright (c) <2013-2014> <Colin Duquesnoy>
-# Copyright (c) <2017-2018> <Michell Stuttgart>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
 """
 A simple example of use.
 
@@ -37,12 +12,12 @@ Requirements:
     the example
 
 """
-import logging
-import unittest
-import sys
-import os
 
-from PyQt5 import QtWidgets, QtCore
+import logging
+import sys
+import unittest
+
+from PyQt5 import QtCore, QtWidgets
 
 import qdarkgraystyle
 from example.ui import example_pyqt5_ui as example_ui
@@ -69,7 +44,7 @@ class TestPyQt5(unittest.TestCase):
         # setup stylesheet
         app.setStyleSheet(qdarkgraystyle.load_stylesheet())
 
-        # auto quit after 2s when testing on travis-ci
+        # auto quit after 2s when testing
         QtCore.QTimer.singleShot(2000, app.exit)
 
         # run
